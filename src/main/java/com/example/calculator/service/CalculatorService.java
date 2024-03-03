80a41b3f95106f31e390a3860df6179b8979a00b
lvarezhkal <kkakxaa@gmail.com>
2024-03-03 14:42:58 +0300

    allDone

---
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.example.calculator.service;
+
+public interface CalculatorService {
+    int sum (int num1, int num2);
+
+    int subtract (int num1, int num2);
+
+    int multiply (int num1, int num2);
+
+    int divide (int num1, int num2);
+
+
+}
